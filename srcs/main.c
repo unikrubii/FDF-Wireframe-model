afd1cbfa629c43bb778dd8e1bee4b40f9d3a4f71
unikrubii <gamez.zeta@gmail.com>
2022-06-21 22:49:45 +0700

    add check file not found

--- srcs/main.c
+++ srcs/main.c
@@ -6,11 +6,27 @@
 /*   By: sthitiku <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/18 23:55:48 by sthitiku          #+#    #+#             */
-/*   Updated: 2022/06/21 22:15:08 by sthitiku         ###   ########.fr       */
+/*   Updated: 2022/06/21 22:48:42 by sthitiku         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../incs/fdf.h"
+
+static int	check_file(char *file)
+{
+	int		fd;
+	int		ret;
+	int		readf;
+	char	buf[10];
+	
+	ret = 1;
+	fd = open(file, O_RDONLY);
+	readf = read(fd, buf, 10);
+	if (readf < 0)
+		ret = 0;
+	close(fd);
+	return (ret);
+}
 
 static int	ext_correct(char *ext, char *fmt, int len)
 {
@@ -38,6 +54,8 @@ int	main(int ac, char **av)
 		ext = ft_strrchr(av[1], '.');
 		if (!ext_correct(ext, ".fdf", 4))
 			error_msg("Not .fdf file\n");
+		if (!check_file(av[1]))
+			error_msg("File not found\n");
 		init_sc(av[1]);
 	}
 	else
